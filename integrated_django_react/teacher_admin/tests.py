4cee88e68630986343a04329cfe720774f9eafc0
JohnDeVries <jdevries3133@gmail.com>
2020-05-22 16:37:14 -0400

    master merge

--- integrated_django_react/teacher_admin/tests.py
+++ integrated_django_react/teacher_admin/tests.py
@@ -1,3 +1,23 @@
+import uuid
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Gallery
+
+class GalleryConflictResolve(TestCase):
+    def setUp(self):
+        pass
+
+    def test_conflict_avoidance(self):
+        gal_name = uuid.uuid1()
+        print('test run')
+        for i in range(100):
+            Gallery.objects.create(
+                owner='jack',
+                title=gal_name,
+                api_obj=['li'],
+            )
+
+
+
+
