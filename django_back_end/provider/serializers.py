20227176e361ad5da85b7a494dca7c818d5c4586
JohnDeVries <jdevries3133@gmail.com>
2020-05-22 22:43:26 -0400

    deleted redundant files

--- django_back_end/provider/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from teacher.models import Gallery
-
-class PublicGallerySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Gallery
-        fields = ['title', 'description', 'api_obj']
--- django_back_end/provider/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-from teacher.models import Gallery
-from .serializers import PublicGallerySerializer
-
-def public_gallery(request, public_id):
-    queryset = Gallery.objects.filter(public_id=public_id)
-    breakpoint()
