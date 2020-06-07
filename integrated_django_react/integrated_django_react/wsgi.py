6a78de15c1ea545eb38ae048280ac9bd06713df7
Ubuntu <ubuntu@ip-172-26-6-145.ec2.internal>
2020-06-07 02:13:58 +0000

    working very basic apache conf

--- integrated_django_react/integrated_django_react/wsgi.py
+++ integrated_django_react/integrated_django_react/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import json
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'integrated_django_react.settings')
+
+# My env variables
+with open('/home/ubuntu/main/integrated_django_react/integrated_django_react/config.json', 'r') as jsn:
+    env = json.load(jsn)
+
+    for k, v in env.items():
+        os.environ.setdefault(k, v)
 
 application = get_wsgi_application()
