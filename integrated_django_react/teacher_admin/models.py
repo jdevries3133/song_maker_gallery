ca68ee034ba4df97ed652e13c155d81441a12048
JohnDeVries <jdevries3133@gmail.com>
2020-09-26 21:21:39 -0400

    Revised unusual save function from Gallery model

--- integrated_django_react/teacher_admin/models.py
+++ integrated_django_react/teacher_admin/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django_mysql.models import JSONField
 
+
 class Gallery(models.Model):
     owner = models.ForeignKey(
         User,
@@ -19,7 +20,7 @@ class Gallery(models.Model):
     )
     url_extension = models.CharField(
         max_length=100,
-        default='err',  # will be assigned by save overwrite
+        default='unassigned',  # will be assigned by save overwrite
         blank=True,
         primary_key=True,
     )
@@ -36,44 +37,56 @@ class Gallery(models.Model):
         # anytime you try to update the model, it becomes duplicated with a new
         # url extension, and the old one just hangs around. Plus, I think the
         # api serializer.data ends up being wrong because of this, too.
-        self.__dict__.setdefault('custom_overwrite', False)
-        if not self.custom_overwrite:
-            self.url_extension = self.title.lower().replace(' ', '-')
-            outstr = ''
-            for i in self.url_extension:
-                if re.search(r'[a-zA-Z0-9\-]', i):
-                    outstr += i
-            self.url_extension = outstr
-            conflicting_urls = [i.url_extension for i in Gallery.objects.filter(url_extension__contains=self.url_extension)]
-            if conflicting_urls:
-                append_int = 1
-                while True:
-                    new_url = self.url_extension + str(append_int)
-                    if new_url in conflicting_urls:
-                        append_int += 1
-                    else:
-                        self.url_extension = new_url
-                        break
+        if self.url_extension != 'unassigned':
+            super().save(args, kwargs)
+            return
+        self.url_extension = self.title.lower().replace(' ', '-')
+        outstr = ''
+        for i in self.url_extension:
+            if re.search(r'[a-zA-Z0-9\-]', i):
+                outstr += i
+        self.url_extension = outstr
+        conflicting_urls = [i.url_extension for i in Gallery.objects.filter(
+            url_extension__contains=self.url_extension)]
+        if conflicting_urls:
+            append_int = 1
+            while True:
+                new_url = self.url_extension + str(append_int)
+                if new_url in conflicting_urls:
+                    append_int += 1
+                else:
+                    self.url_extension = new_url
+                    break
 
-            # convert names to first name, last initial with proper case
-            # also, insert placeholder image
-            for group in self.api_obj:
-                for index, row in enumerate(group[:-1]):
+        # convert names to first name, last initial with proper case
+        # also, insert placeholder image
+        for group in self.api_obj:
+            for index, row in enumerate(group[:-1]):
 
-                    # fixing names
-                    full_name = row[0]
-                    name_arr = full_name.split(' ')
-                    if len(name_arr) > 1:
-                        name= name_arr[0].title() + ' ' + name_arr[1][0].upper() + '.'
-                    else:
-                        name = name_arr[0]
+                # fixing names
+                full_name = row[0]
+                name_arr = full_name.split(' ')
+                if len(name_arr) > 1:
+                    name = name_arr[0].title() + ' ' + \
+                        name_arr[1][0].upper() + '.'
+                else:
+                    name = name_arr[0]
+                try:
                     group[index][0] = name
+                except (AttributeError, TypeError):
+                    raise Exception(
+                        'Save method was run on an object that was already'
+                        'created. This means that the if not self.url_extension'
+                        'check at the beginning of Gallery.save() failed.'
+                    )
 
-                    # insert placeholder image
-                    row.append('https://song-maker-gallery.s3.amazonaws.com/manually_added/Placeholder.png')
+                # insert placeholder image
+                row.append(
+                    'https://song-maker-gallery.s3.amazonaws.com/manually_added'
+                    '/Placeholder.png'
+                )
 
-        super().save(*args, **kwargs)
-
+        super().save(args, kwargs)
 
     def __str__(self):
         return self.title
--- integrated_django_react/teacher_admin/tests.py
+++ integrated_django_react/teacher_admin/tests.py
@@ -1,21 +1,37 @@
-import uuid
+import json
 
 from django.test import TestCase
 from django.contrib.auth.models import User
 
 from .models import Gallery
 
+
 class GalleryConflictResolve(TestCase):
     def setUp(self):
-        user = User.objects.create_user('jack', 'jack@jack.com', 'jackpassword')
-        for i in range(100):
+        user = User.objects.create_user(
+            'jack', 'jack@jack.com', 'jackpassword')
+        for _ in range(100):
+            with open('teacher_admin/test_data.json', 'r') as jsn:
+                obj = json.load(jsn)
             Gallery.objects.create(
                 owner=user,
                 title='Test gallery Name',
-                api_obj=['li'],
+                api_obj=obj,
             )
 
-    def test_conflict_avoidance(self):
-        objs = Gallery.objects.all()
-        for obj in objs:
+    def get_queryset(self):
+        return Gallery.objects.all()
+
+    def test_url_naming_conflict_avoidance(self):
+        for obj in self.get_queryset():
             self.assertEqual(obj.url_extension[:17], 'test-gallery-name')
+
+    def test_url_does_not_mutate_on_update(self):
+        for obj in self.get_queryset():
+            # check that the url does not change on update
+            original_url = obj.url_extension
+            obj.api_obj = ['this']
+            obj.save()
+            obj.refresh_from_db()
+            new_url = obj.url_extension
+            self.assertEqual(original_url, new_url)
