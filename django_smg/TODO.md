884b52beeb620c792bf4bad85827c076a48ca205
JohnDeVries <jdevries3133@gmail.com>
2020-12-30 23:47:54 -0500

    Include hard coded sample gallery in the frontend

--- django_smg/TODO.md
+++ django_smg/TODO.md
@@ -5,12 +5,15 @@
 - [x] Update frontend state management to expect songData
 - [x] Make backend gallery delete endpoint
 - [x] Fetching and caching service
-- [ ] Hook sample gallery into django migration
-- [ ] Include hard coded sample gallery in the frontend
+- [ ] ~~Hook sample gallery into django migration~~
+- [x] Include hard coded sample gallery in the frontend
 - [ ] Make endpoint at `/api/login/ping-auth/` for the frontend to try out an
       old token
 - [x] Fix `/api/gallery/undefined` bug.
 - [x] Fix post-delete gallery dialogue box is not dismissed
-- [ ] Update "mobile optimized view"
+- [ ] Update "mobile optimized view" for Teacher component
 - [ ] Fix group name not set properly by frontend
   - Should be the last row in the group.
+- [ ] Make it so the donation button automatically doesn't appear for the
+      galleries of privileged users.
+- [ ] Change Tile css to use `background-color`
--- django_smg/frontend/src/components/landing_page/landing_page.jsx
+++ django_smg/frontend/src/components/landing_page/landing_page.jsx
@@ -1,49 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import GalleryBody from "../gallery/gal_body";
 import Welcome from "./welcome/welcome";
-import Loading from "../generics/loading";
-import { connect } from "react-redux";
-import { getGallery } from "../../actions/gallery";
 import styles from "./landing_page.module.css";
 
+import sampleGallery from "./sample-gallery.json";
+
 const landing_page = (props) => {
-  useEffect(() => {
-    props.getGallery("1"); // warning: make sure you take sample-gallery with you into new databases
-  }, []);
-  if (props.status == undefined) {
-    return (
-      <div>
-        <Welcome />
-        <div className={styles.space} />
-        <Loading />
-      </div>
-    );
-  } else {
-    if (props.status != 200) {
-      return (
-        <div>
-          <h1>Internal Server Error</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <Welcome />
-          <div className={styles.space} />
-          <GalleryBody
-            title={props.gallery.title}
-            description={props.gallery.description}
-            data={props.gallery.api_obj}
-          />
-        </div>
-      );
-    }
-  }
+  return (
+    <div>
+      <Welcome />
+      <div className={styles.space} />
+      <GalleryBody
+        title={sampleGallery.title}
+        description={sampleGallery.description}
+        data={sampleGallery.songData}
+      />
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => {
-  return { gallery: state.gallery.gallery, status: state.gallery.status };
-};
-
-export default connect(mapStateToProps, { getGallery })(landing_page);
+export default landing_page;
