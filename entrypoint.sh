1f008b2e59c3b86f8988aa48c4f3117dfc4394d8
Jack DeVries <jdevries3133@gmail.com>
2021-01-11 04:40:50 +0000

    fix: remove accidentally commit files from server

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python wait_for_db.py
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-python init_django.py
-
-exec gunicorn integrated_django_react.wsgi:application --bind 0.0.0.0:8000
