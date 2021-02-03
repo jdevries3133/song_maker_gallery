2090e5eeff8e439f8198bb7eec2035c72c558d27
Jack DeVries <jdevries3133@gmail.com>
2021-02-03 20:32:23 +0000

    feat: sql backup script runnable by cron

---
+++ django_smg/backup_sql.sh
@@ -0,0 +1,6 @@
+# Script to create simple sql backups as sqldumps on disk
+
+cd /home/ubuntu/song_maker_gallery/django_smg
+source venv/bin/activate
+mv sqldumps/dump.sql sqldumps/dump_old.sql
+python manage.py dumpdata > sqldumps/dump.sql
