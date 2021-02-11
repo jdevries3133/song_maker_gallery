98c0b7519b207dde5348052030e0e51dad3b2c74
Jack DeVries <jdevries3133@gmail.com>
2021-02-10 19:30:36 -0500

    fix: deploy.sh does not scp twice

--- django_smg/frontend/deploy.sh
+++ django_smg/frontend/deploy.sh
@@ -2,5 +2,4 @@
 
 source ../venv/bin/activate
 npm run build
-scp static/frontend/webpack_output/main*.js ubuntu@ec2:/home/ubuntu/song_maker_gallery/django_smg/static_root/frontend/webpack_output/
 scp static/frontend/webpack_output/* ubuntu@ec2:/home/ubuntu/song_maker_gallery/django_smg/static_root/frontend/webpack_output/
