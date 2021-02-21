42db31bf1791a44e05aea0c34dc00b5b8b1e7213
Jack DeVries <jdevries3133@gmail.com>
2021-02-21 00:00:00 +0000

    feat: deploy.sh sends the frontend bundle to two server destinations

--- django_smg/frontend/deploy.sh
+++ django_smg/frontend/deploy.sh
@@ -3,3 +3,4 @@
 source ../venv/bin/activate
 npm run build
 scp static/frontend/webpack_output/* ubuntu@ec2:/home/ubuntu/song_maker_gallery/django_smg/static_root/frontend/webpack_output/
+scp static/frontend/webpack_output/* ubuntu@ec2:/home/ubuntu/song_maker_gallery/django_smg/frontend/static/frontend/webpack_output/
