d31fd99428f273bc5e21321a8a71f9cc71502c85
Jack DeVries <jdevries3133@gmail.com>
2021-03-13 00:00:00 +0000

    fix: dev db scripts

--- django_smg/dev_db/rm_db.sh
+++ django_smg/dev_db/rm_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker ps | grep "songmaker-dev-db"  | cut -c 1-10 | xargs docker kill | xargs docker rm
+docker ps --all | grep "songmaker-dev-db"  | cut -c 1-10 | xargs docker kill | xargs docker rm
