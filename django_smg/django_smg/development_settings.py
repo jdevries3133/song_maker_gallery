d47dbeac56f666b0d4d0c428a88b73277ed703b8
Jack DeVries <jdevries3133@gmail.com>
2021-03-15 00:00:00 +0000

    feat(\#27): update configuraiton to support emailing

--- django_smg/django_smg/development_settings.py
+++ django_smg/django_smg/development_settings.py
@@ -3,6 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- django_smg/django_smg/production_settings.py
+++ django_smg/django_smg/production_settings.py
@@ -3,11 +3,14 @@ import os
 from .settings import BASE_DIR
 
 # Email
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = config['SMG_GMAIL']
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = config['SMTP_HOST']
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+from .secret_settings import (
+    EMAIL_HOST_USER,
+    EMAIL_HOST_PASSWORD
+)
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 from .secret_settings import (
     MYSQL_PASSWORD,
