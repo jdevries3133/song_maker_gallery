4e21ca04a14b357926f9161500c71b897820643e
Jack DeVries <jdevries3133@gmail.com>
2021-03-18 00:00:00 +0000

    feat: frontend deploy script clears previous output

--- django_smg/frontend/deploy.sh
+++ django_smg/frontend/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm static/frontend/webpack_output/*
 source ../venv/bin/activate
 npm run build
 scp static/frontend/webpack_output/* ubuntu@ec2:/home/ubuntu/song_maker_gallery/django_smg/static_root/frontend/webpack_output/
