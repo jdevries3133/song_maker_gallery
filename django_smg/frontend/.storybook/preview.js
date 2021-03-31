12b74270cc566ab727502810f4f822da513bed5c
Jack DeVries <jdevries3133@gmail.com>
2021-03-31 00:00:00 +0000

    feat: storybook previews include global app styles

--- django_smg/frontend/.storybook/preview.js
+++ django_smg/frontend/.storybook/preview.js
@@ -1,3 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+/**
+ * These are the same styles from '../static/frontend/global_styles.css'.
+ * This is kind of a hacky way of getting them into storybook because I have
+ * just been suffering with storybook config for far too long.
+ */
+const Div = styled.div`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+  background: url("/static/frontend/background.png");
+  background-size: 50vw;
+  font-family: "Merriweather Sans", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+    "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  @media (min-width: 475px) {
+    scroll-behavior: smooth;
+  }
+`;
+
+export const decorators = [
+  (Story) => (
+    <Div>
+      <Story />
+    </Div>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,4 +42,5 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
