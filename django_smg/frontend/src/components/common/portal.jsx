e30b2ec72d0dc7176ccd78412803f8846fbaa161
Jack DeVries <jdevries3133@gmail.com>
2021-04-02 00:00:00 +0000

    feat: <Portal /> injects global styles to make it work in storybook

--- django_smg/frontend/src/components/common/portal.jsx
+++ django_smg/frontend/src/components/common/portal.jsx
@@ -1,5 +1,36 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
+
+/* Hack because global css doesn't work in storybook... */
+const GlobalStyles = styled.div`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+  background: url("/static/frontend/background.png");
+  background-size: 50vw;
+  font-family: "Merriweather Sans", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+    "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  @media (min-width: 475px) {
+    scroll-behavior: smooth;
+  }
+`;
+
+/* Necessary because this div should cover the whole screen */
+const FixedWrapper = styled(GlobalStyles)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vh;
+  height: 100vh;
+`;
 
 /* Makes an element that jest / react-testing library can test against */
 const getElement = () => {
@@ -23,5 +54,7 @@ export const Portal = ({ children }) => {
       document.body.removeChild(document.getElementById("portalContainer"));
     };
   }, []);
-  return modalEl ? ReactDOM.createPortal(children, modalEl) : null;
+  return modalEl
+    ? ReactDOM.createPortal(<FixedWrapper>{children}</FixedWrapper>, modalEl)
+    : null;
 };
