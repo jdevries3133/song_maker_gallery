3956c886ce3f1c1d71131933e0fc4dc64d448170
Jack DeVries <jdevries3133@gmail.com>
2021-04-03 00:00:00 +0000

    fix: bug in usage of window.location wrapper func

--- django_smg/frontend/src/components/teacher/list_galleries/user_galleries.jsx
+++ django_smg/frontend/src/components/teacher/list_galleries/user_galleries.jsx
@@ -44,7 +44,7 @@ const GalleryRows = ({ galleries, requestDelete }) =>
             color="#fa8071"
             onClick={() =>
               requestDelete({
-                url: `${windowLocation}galHref`,
+                url: `${windowLocation("origin")}galHref`,
                 pk: gallery["pk"],
               })
             }
