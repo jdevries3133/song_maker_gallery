2190f3cc373936a1e75a12b425782b3a04d062d9
Jack DeVries <jdevries3133@gmail.com>
2021-05-04 00:00:00 +0000

    test: gotta go fast

--- django_smg/django_smg/settings/test.py
+++ django_smg/django_smg/settings/test.py
@@ -1,29 +1,11 @@
-"""
-This is injected last automatically when you run tests with manage.py. It just
-suppresses log messages because ideally, they don't need to be seen in most
-cases.
-"""
 from .base import *
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'handlers': {
-        'supress': {
-            'level': 'CRITICAL',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    '': {
-        'handlers': ['supress'],
-        'level': 'CRITICAL',
-    },
-    'root': {
-        'handlers': ['supress'],
-        'level': 'CRITICAL',
-    },
-    'django': {
-        'handlers': ['supress'],
-        'level': 'CRITICAL'
-    },
-}
+# Quiet logging
+import logging
+logging.disable(logging.CRITICAL)
+
+# Gotta go fast
+DEBUG = False
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
