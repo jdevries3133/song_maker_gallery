124c1658d9e5efdb2c839357e1283b75786ad3ad
Jack DeVries <jdevries3133@gmail.com>
2021-05-14 00:00:00 +0000

    fix: gallery_form is dependent on backend due to use of LiveUpdatingTile

--- django_smg/frontend/src/components/teacher/gallery_form/gallery_form.stories.jsx
+++ django_smg/frontend/src/components/teacher/gallery_form/gallery_form.stories.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
+import { BackendDependentStory } from "Common/storybook_util";
+
 import { GalleryForm } from "./gallery_form";
 import testData from "Test/test_data";
+
 export default {
   title: "Teacher/Gallery Form",
   component: GalleryForm,
 };
 
-const Template = (args) => <GalleryForm {...args} />;
+const Template = (args) => (
+  <BackendDependentStory>
+    <GalleryForm {...args} />
+  </BackendDependentStory>
+);
 
 export const Default = Template.bind({});
 Default.args = testData;
