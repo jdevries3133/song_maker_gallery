e6cdf2e551419f3b1577a4a70521f858103d3a6e
Jack DeVries <jdevries3133@gmail.com>
2021-11-24 15:46:59 -0500

    remove unused social gallery feature code

--- django_smg/frontend/src/components/teacher/gallery_config/gallery_config_form.jsx
+++ django_smg/frontend/src/components/teacher/gallery_config/gallery_config_form.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 
-import styled, { Description as DefaultDescription, H2, P, Form } from "Styles";
+import styled, {Description as DefaultDescription, H2, P, Form} from "Styles";
 
-import { ConfigItem } from "Common/config_item";
+import {ConfigItem} from "Common/config_item";
 
 const Description = styled(DefaultDescription)`
   text-align: inherit;
@@ -13,11 +13,10 @@ const Description = styled(DefaultDescription)`
 export const GalleryConfigForm = ({
   state,
   onCheckedHandler,
-  onSubmitHandler,
   renderExtraForm,
   slug = null,
 }) => (
-  <Form onSubmit={onSubmitHandler}>
+  <Form onSubmit={(e) => e.preventDefault()}>
     <Description>
       <H2>Gallery Settings</H2>
       {state.allowStudentSubmissions.checked &&
@@ -33,6 +32,8 @@ export const GalleryConfigForm = ({
           label="Allow Student Submissions"
           checked={state.allowStudentSubmissions.checked}
           onChange={() => onCheckedHandler(state.allowStudentSubmissions.id)}
+            onCheckedHandler(state?.checkboxes.allowStudentSubmissions.id)
+          }
         >
           Enable students to actively submit compositions{" "}
           {slug ? (
@@ -45,8 +46,10 @@ export const GalleryConfigForm = ({
         <ConfigItem
           id={state.isGalleryPublished.id}
           label="Gallery is Public"
-          checked={state.isGalleryPublished.checked}
-          onChange={() => onCheckedHandler(state.isGalleryPublished.id)}
+          checked={state?.checkboxes.isGalleryPublished?.checked}
+          onChange={() =>
+            onCheckedHandler(state?.checkboxes.isGalleryPublished.id)
+          }
         >
           When you switch this off, the gallery goes into private mode. This is
           helpful if you want to withhold the gallery leading up to a release,
@@ -56,82 +59,12 @@ export const GalleryConfigForm = ({
         </ConfigItem>
       </div>
     </Description>
-
-    {/* <Description> */}
-    {/*   <H2>Social Gallery Features</H2> */}
-
-    {/*   <div> */}
-    {/*     <ConfigItemSet> */}
-    {/*       <ConfigItem */}
-    {/*         id={state.enableSocialGal.id} */}
-    {/*         label="Enable Social Gallery" */}
-    {/*         checked={state.enableSocialGal.checked} */}
-    {/*         onChange={() => checkedHandler(state.enableSocialGal.id)} */}
-    {/*       > */}
-    {/*         Flip this switch to turn this gallery into a social gallery, and */}
-    {/*         to gain access to the finer grain settings below. */}
-    {/*       </ConfigItem> */}
-
-    {/*       <ConfigItem */}
-    {/*         id={state.socialGalPasscodeEnabled.id} */}
-    {/*         label="Social Gallery Passcode" */}
-    {/*         checked={state.socialGalPasscodeEnabled.checked} */}
-    {/*         onChange={() => checkedHandler(state.socialGalPasscodeEnabled.id)} */}
-    {/*       > */}
-    {/*         {state.socialGalPasscodeEnabled.checked ? ( */}
-    {/*           <> */}
-    {/*             <P left> */}
-    {/*               Students must enter this passcode to interact with the */}
-    {/*               social gallery */}
-    {/*             </P> */}
-    {/*             <InlineLabel htmlFor="socialGalleryPasscode"> */}
-    {/*               Passcode */}
-    {/*             </InlineLabel> */}
-    {/*             <Input */}
-    {/*               id="socialGalleryPasscode" */}
-    {/*               type="text" */}
-    {/*               value={socialGalleryPasscode} */}
-    {/*               onChange={(e) => setSocialGalleryPasscode(e.target.value)} */}
-    {/*             /> */}
-    {/*           </> */}
-    {/*         ) : null} */}
-    {/*       </ConfigItem> */}
-
-    {/*       <ConfigItem */}
-    {/*         id={state.reactionsEnabled.id} */}
-    {/*         label="Enable Reactions" */}
-    {/*         checked={state.reactionsEnabled.checked} */}
-    {/*         onChange={() => checkedHandler(state.reactionsEnabled.id)} */}
-    {/*       > */}
-    {/*         <Emoji>👍</Emoji> */}
-    {/*         <Emoji>❤️</Emoji> */}
-    {/*         <Emoji>⭐️</Emoji> */}
-    {/*       </ConfigItem> */}
-
-    {/*       <ConfigItemSet> */}
-    {/*         <ConfigItem */}
-    {/*           id={state.commentsEnabled.id} */}
-    {/*           label="Enable Comments" */}
-    {/*           checked={state.commentsEnabled.checked} */}
-    {/*           onChange={() => checkedHandler(state.commentsEnabled.id)} */}
-    {/*         /> */}
-    {/*         <ConfigItem */}
-    {/*           id={state.moderateComments.id} */}
-    {/*           label="Pre-Approve All Comments" */}
-    {/*         > */}
-    {/*           No comments will be viewable until they are approved by you. */}
-    {/*         </ConfigItem> */}
-    {/*       </ConfigItemSet> */}
-    {/*     </ConfigItemSet> */}
-    {/*   </div> */}
-    {/* </Description> */}
     {renderExtraForm}
   </Form>
 );
 
 GalleryConfigForm.propTypes = {
   onCheckedHandler: PropTypes.func.isRequired,
-  onSubmitHandler: PropTypes.func.isRequired,
   state: PropTypes.shape({
     allowStudentSubmissions: PropTypes.shape({
       id: PropTypes.string,
