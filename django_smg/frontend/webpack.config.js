f91e4c107a06afc1a2f5776eb181589a20af3a21
Jack DeVries <jdevries3133@gmail.com>
2021-12-01 22:11:34 -0500

    bump version to 3.1.1

--- django_smg/frontend/webpack.config.js
+++ django_smg/frontend/webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
   },
   entry: "./src/index.jsx",
   output: {
-    filename: "main_v3.1.0.js",
+    filename: "main_v3.1.1.js",
     path: path.resolve(__dirname, "static", "frontend", "webpack_output"),
   },
 };
